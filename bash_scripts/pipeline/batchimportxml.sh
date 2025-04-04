8047e6be2bd425c42e0987e6422cd3dca1ec7c07
Andrew Wint <23616783+andrewwint@users.noreply.github.com>
2025-04-04 09:07:27 -0400

    Update batchimportxml.sh

--- bash_scripts/pipeline/batchimportxml.sh
+++ bash_scripts/pipeline/batchimportxml.sh
@@ -8,13 +8,13 @@ else
   DELETEXML=''  
 fi
 
-if [ -d /c/Program\ Files\ \(x86\)/Apache\ Software\ Foundation/Apache2.2/htdocs/truechoice/app/clients/prototype/$1/xml/ ];then
+if [ -d /c/Program\ Files\ \(x86\)/Apache\ Software\ Foundation/Apache2.2/htdocs/app/clients/prototype/$1/xml/ ];then
   rm -rf ./xml/*
-  cp -r /c/Program\ Files\ \(x86\)/Apache\ Software\ Foundation/Apache2.2/htdocs/truechoice/app/clients/prototype/$1/xml/* ./xml/
+  cp -r /c/Program\ Files\ \(x86\)/Apache\ Software\ Foundation/Apache2.2/htdocs/app/clients/prototype/$1/xml/* ./xml/
   $(./base64xmls.sh ./xml/)
   JSON=$(cat ./tmp-xmls.txt)
   echo "{\"server\":\"$2\", \"deleteXml\":\"${DELETEXML}\", \"payload\":{${JSON:0:-1}}}" > ./tmp-batch.txt 
-  echo $(curl -X POST "https://webservices.truechoice.io/xmlimport/batchxml" -# -H "accept: */*" -H "Content-Type: application/json" -d @tmp-batch.txt)
+  echo $(curl -X POST "https://webservices.io/xmlimport/batchxml" -# -H "accept: */*" -H "Content-Type: application/json" -d @tmp-batch.txt)
   #echo $(curl -X POST "http://localhost:3000/xmlimport/batchxml" -# -H "accept: */*" -H "Content-Type: application/json" -d @tmp-batch.txt)
 
   unlink ./tmp-xmls.txt
