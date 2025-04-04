7f9129c061880794d58f5f918ab3e2f2ed01c6f8
Andrew Wint <23616783+andrewwint@users.noreply.github.com>
2025-04-04 09:07:07 -0400

    Update importxml.sh

--- bash_scripts/pipeline/importxml.sh
+++ bash_scripts/pipeline/importxml.sh
@@ -11,7 +11,7 @@ fi
 if [ -e $1 ];then
   xmlbase64="`cat $1 | base64`"
   echo "{\"server\":\"$2\", \"deleteXml\":\"${DELETEXML}\", \"payload\":\"${xmlbase64}\"}" > ./tmp.txt 
-  echo $(curl -X POST "https://webservices.truechoice.io/xmlimport/xml" -H "accept: */*" -H "Content-Type: application/json" -d @tmp.txt)
+  echo $(curl -X POST "https://webservices.io/xmlimport/xml" -H "accept: */*" -H "Content-Type: application/json" -d @tmp.txt)
   #echo $(curl -X POST "http://localhost:3000/xmlimport/xml" -H "accept: */*" -H "Content-Type: application/json" -d @tmp.txt)
   unlink ./tmp.txt 
 else
